8bd15d630ca44145d683f2618387f1d93bb696ac
seongmo-yoon <momoysm@gmail.com>
2024-05-16 21:59:53 +0900

    ExceptionHandler 추가 수정

--- src/main/java/com/sparta/schedulemanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/schedulemanagement/exception/GlobalExceptionHandler.java
@@ -1,20 +1,31 @@
 package com.sparta.schedulemanagement.exception;
 
 import com.sparta.schedulemanagement.dto.ExceptionDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class Exception {
+public class GlobalExceptionHandler {
+
+    private final Logger logger;
+
+    public GlobalExceptionHandler() {
+        this.logger = LoggerFactory.getLogger(GlobalExceptionHandler.class.getName());
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionDto> handleException(IllegalArgumentException exception){
+
         String message = exception.getMessage();
 
         ExceptionDto exceptionDto = new ExceptionDto(message);
 
         ResponseEntity<ExceptionDto> responseEntity = ResponseEntity.badRequest().body(exceptionDto);
+
+        logger.error(exception.getMessage());
 
         return responseEntity;
     }
